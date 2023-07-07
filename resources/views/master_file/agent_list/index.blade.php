56151723323ce3b7bc1bd17747ea7ba2f9ffa9cf
EmmanuelMojares <136673148+EmmanuelMojares@users.noreply.github.com>
2023-07-07 11:14:15 +0800

    new update

--- resources/views/master_file/agent_list/index.blade.php
+++ resources/views/master_file/agent_list/index.blade.php
@@ -12,9 +12,61 @@
 
     <div class="card bg-white">
         @include('components.filter')
+                <!-- Modal toggle -->
+<button data-modal-target="staticModal" data-modal-toggle="staticModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+  + ADD 
+</button>
+
+<!-- Main modal -->
+<div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Add Client
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form action="#">
+                        <div class="gap-5 ml-4 mr-4 sm:grid-cols-2">
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Client Name</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Client Address</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">View</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+            <!-- Modal footer -->
+              <div class="flex items-center justify-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="staticModal" type="button" class="justify-end text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-hide="staticModal" type="button" class="justify-end text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
+        </div>
     </div>
+</div>    
+    </div>
+     
 
     <div class="card bg-white relative overflow-x-auto shadow-md sm:rounded-lg">
+
+        <div class="card bg-white relative overflow-x-auto shadow-md sm:rounded-lg">
         <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
@@ -22,46 +74,41 @@
           <h1 class="text-gray-800 text-1xl">Agent Data Table</h1>
         </div>
 
-        <!-- button -->
-        <div class="mt-6 mb-3 flex items-start justify-start">
-            <button class="w-40 py-3 px-4  text-center bg-indigo-600 rounded-md text-white text-sm hover:bg-indigo-500"
-                data-bs-toggle="modal" data-bs-target="#addCollector"><i class="fa fa-plus"></i> Add </button>
-        </div>
-
         @include('components.export')
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        Name
+                         Name
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Address
+                         Address
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Valid Number
+                         Valid Number
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        View
+                         View
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Action
+                         Action
                     </th>
                 </tr>
             </thead>
+
             <tbody>
                 <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Emmanuel Robillos Mojares
+                        emman
                     </th>
                     <td class="px-6 py-4">
-                        Saranay blk 1 lot 7 jasmine streer bagumbong caloocan city
+                        rainbow village 
                     </td>
                     <td class="px-6 py-4">
-                        01-27-22435
+                        01-20-01
                     </td>
                     <td class="px-6 py-4">
-                        Brgy 178
+                        Cash
                     </td>
                     <td class="px-6 py-4">
                         <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
--- resources/views/master_file/collector_list/collection.blade.php
+++ resources/views/master_file/collector_list/collection.blade.php
@@ -12,8 +12,57 @@
 
     <div class="card bg-white">
         @include('components.filter')
-    </div>
 
+        <!-- Modal toggle -->
+<button data-modal-target="staticModal" data-modal-toggle="staticModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+  + ADD 
+</button>
+
+<!-- Main modal -->
+<div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Add Client
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form action="#">
+                        <div class="gap-5 ml-4 mr-4 sm:grid-cols-2">
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Client Name</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Client Address</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contact Number</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+                            <div>
+                                <label for="loan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Form Number</label>
+                                <input type="text" name="loan_id" id="loan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" required="">
+                            </div>
+            <!-- Modal footer -->
+            <div class="flex items-center justify-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="staticModal" type="button" class="justify-end text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-hide="staticModal" type="button" class="justify-end text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
+        </div>
+    </div>
+</div>    
+    </div>
 
     <div class="card bg-white relative overflow-x-auto shadow-md sm:rounded-lg">
 
@@ -23,12 +72,6 @@
       <div class="row mb-2">
         <div class="col-sm-6">
           <h1 class="text-gray-800 text-1xl">Data Table</h1>
-        </div>
-
-        <!-- button -->
-        <div class="mt-6 mb-3 flex items-start justify-start">
-            <button class="w-40 py-3 px-4  text-center bg-indigo-600 rounded-md text-white text-sm hover:bg-indigo-500"
-                data-bs-toggle="modal" data-bs-target="#addCollector"><i class="fa fa-plus"></i> Add </button>
         </div>
 
         @include('components.export')
